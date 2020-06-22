e8cf537cf518332a31474f36133720623b2db8c6
Robert Schrenk <robert@schrenk.cc>
2020-06-22 13:55:55 +0200

    bump version.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020060900;
+$plugin->version   = 2020062200;
 $plugin->requires  = 2014051200;
 $plugin->component = 'mod_msteams';
 $plugin->cron      = 0;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0 (Build: 2020060900)';
+$plugin->release   = '1.0 (Build: 2020062200)';
