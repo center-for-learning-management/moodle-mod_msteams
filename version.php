6c1d605ade167435c7a16ce689957a2d9bf30fd6
Robert Schrenk <robert@schrenk.cc>
2021-07-21 18:02:37 +0200

    increment version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021062300;
+$plugin->version   = 2021072100;
 $plugin->requires  = 2014051200;
 $plugin->component = 'mod_msteams';
 $plugin->cron      = 0;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2';
+$plugin->release   = '1.3';
